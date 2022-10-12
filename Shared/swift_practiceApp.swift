0efeda2879a4ea79a708920f4a34803d87fd4617
ji-hyun219 <ix219@hanmail.net>
2022-10-12 22:38:17 +0900

    Initial Commit

---
+++ Shared/swift_practiceApp.swift
@@ -0,0 +1,17 @@
+//
+//  swift_practiceApp.swift
+//  Shared
+//
+//  Created by 이지현 on 2022/10/12.
+//
+
+import SwiftUI
+
+@main
+struct swift_practiceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
